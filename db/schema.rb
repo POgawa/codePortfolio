3f2cf87f55d02003c947ce9ecbfa38bdb1354097
Paul Ogawa <student@epicodus.com>
2015-07-02 11:57:25 -0700

    Add comment model and tests. All tests passing

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150702185209) do
+ActiveRecord::Schema.define(version: 20150702185456) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "comment"
+    t.integer  "user_id"
+    t.integer  "project_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string   "title"
