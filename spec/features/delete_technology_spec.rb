b2b8aa0680d64e1d3c4e6188e77d44ec33e02095
Paul Ogawa <student@epicodus.com>
2015-07-02 14:15:52 -0700

    Add delete functionality and integration specs. All tests passing

---
+++ spec/features/delete_technology_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+
+describe "adds a technology to the site" do
+  it "adds a new technology" do
+    visit technologies_path
+    click_on 'Sign-in'
+    click_on 'Sign up'
+    fill_in 'Email', :with => 'test1@email.com'
+    fill_in 'Password', :with => 'password'
+    fill_in 'Password confirmation', :with => 'password'
+    click_on 'Sign up'
+    click_on 'Add technology'
+    fill_in 'Title', :with => 'Technology title'
+    fill_in 'Description', :with => 'Technology description'
+    click_on 'Create Technology'
+    click_on 'Technology title'
+    click_on 'Delete'
+    expect(page).to have_content 'Technological Might'
+  end
+end
